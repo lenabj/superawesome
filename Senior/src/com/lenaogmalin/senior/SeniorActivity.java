45abe0f15d8d658a2df89fb7d6b5f70bf80ed79b
Lena Bjørnstu <lenaskolemail@gmail.com>
2011-09-29 10:51:06 +0200

    First

---
+++ Senior/src/com/lenaogmalin/senior/SeniorActivity.java
@@ -0,0 +1,13 @@
+package com.lenaogmalin.senior;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SeniorActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
